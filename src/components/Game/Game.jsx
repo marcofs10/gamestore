926af24f59cb903dee37cf8453c1e4c1b5be769f
marcofs10 <121571271+marcofs10@users.noreply.github.com>
2024-09-07 18:12:23 -0500

    feat: game card hover

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IconPlatform } from '../IconPlatform/IconPlatform.jsx'
 import './Game.css'
 
 export const Game = ({ game, row }) => {
 
+    const [hovered, setHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setHovered(true)
+    }
+
+    const handleMouseLeave = () => {
+        setHovered(false)
+        setThumbnailShown(0)
+    }
+
     return (
-        <div className={`gameWrapper ${row ? 'gameWrapperRow' : 'gameWrapperGrid'}`}>
-            <div className='game'>
+        <div className={`gameWrapper ${row ? 'gameWrapperRow' : 'gameWrapperGrid'}`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}>
+            <div className={`game ${hovered ? 'gameHover' : ''}`}>
                 <div className='wrapper'>
                     <div className="thumbnail" style={{ backgroundImage: `url(${game.thumbnail[0]})` }} />
                     <div>
@@ -21,6 +34,25 @@ export const Game = ({ game, row }) => {
                         <p className='price'>{`$${game.price}`}</p>
                     </section>
                     <h2>{game.name}</h2>
+                    {(hovered) && (
+                        <div>
+                            <img className='wishIcon'
+                                src='/images/Gift.png' />
+                            <ul>
+                                <li>
+                                    <span>Score:</span>
+                                    <span>{game.rating}</span>
+                                </li>
+                                <li>
+                                    <span>Release Date:</span>
+                                    <span>{game.release}</span>
+                                </li>
+                                <li>
+                                    <span>Genres:</span>
+                                    <span>{game.genre}</span>
+                                </li>
+                            </ul>
+                        </div>)}
                 </section>
             </div>
         </div>
