4f8871a084c2a00bc72838feec6892eb77165500
marcofs10 <121571271+marcofs10@users.noreply.github.com>
2024-09-07 21:02:08 -0500

    feat: thumbnail selector

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -5,6 +5,7 @@ import './Game.css'
 export const Game = ({ game, row }) => {
 
     const [hovered, setHovered] = useState(false);
+    const [thumbnailShown, setThumbnailShown] = useState(0);
 
     const handleMouseEnter = () => {
         setHovered(true)
@@ -15,15 +16,32 @@ export const Game = ({ game, row }) => {
         setThumbnailShown(0)
     }
 
+    const handleThumbnailShown = (e) => {
+        if (hovered) {
+            const coordRelativeX = e.clientX - e.target.offsetParent.offsetParent.offsetParent.offsetLeft;
+            const division = e.target.clientWidth / 5;
+            { division > coordRelativeX ? setThumbnailShown(0) : '' }
+            { (division < coordRelativeX) && (coordRelativeX < division * 2) ? setThumbnailShown(1) : '' }
+            { (division * 2 < coordRelativeX) && (coordRelativeX < division * 3) ? setThumbnailShown(2) : '' }
+            { (division * 3 < coordRelativeX) && (coordRelativeX < division * 4) ? setThumbnailShown(3) : '' }
+            { (division * 4 < coordRelativeX) && (coordRelativeX < division * 5) ? setThumbnailShown(4) : '' }
+        }
+    }
+
     return (
         <div className={`gameWrapper ${row ? 'gameWrapperRow' : 'gameWrapperGrid'}`}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}>
             <div className={`game ${hovered ? 'gameHover' : ''}`}>
-                <div className='wrapper'>
-                    <div className="thumbnail" style={{ backgroundImage: `url(${game.thumbnail[0]})` }} />
-                    <div>
-                        <div className='divActive'></div>
+                <div className='wrapper'
+                    onMouseMove={handleThumbnailShown}>
+                    <div className="thumbnail" style={{ backgroundImage: `url(${game.thumbnail[thumbnailShown]})` }} />
+                    <div className={`${hovered ? 'thumbnailSelector' : ''}`}>
+                        <div className={thumbnailShown === 0 ? 'divActive' : ''}></div>
+                        <div className={thumbnailShown === 1 ? 'divActive' : ''}></div>
+                        <div className={thumbnailShown === 2 ? 'divActive' : ''}></div>
+                        <div className={thumbnailShown === 3 ? 'divActive' : ''}></div>
+                        <div className={thumbnailShown === 4 ? 'divActive' : ''}></div>
                     </div>
                 </div>
                 <section className='info'>
