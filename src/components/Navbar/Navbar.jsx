b38f4128e209802d5a365059889b903074dbe8e0
marcofs10 <121571271+marcofs10@users.noreply.github.com>
2024-09-07 22:33:16 -0500

    feat: added cart animation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import github from '/images/github.webp'
 import cart from '/images/cart.png'
@@ -6,9 +7,26 @@ import './Navbar.css'
 
 export const Navbar = ({ isMobile, cartCount, setCartDisplayed }) => {
 
+    const [prevCount, setPrevCount] = useState(cartCount);
+    const [triggAnimation, setTriggAnimation] = useState(false)
+
     const handleOpenCart = () => {
         setCartDisplayed(true)
     }
+
+    useEffect(() => {
+        if (cartCount > prevCount) {
+            setTriggAnimation(true)
+        }
+        setPrevCount(cartCount)
+    }, [cartCount])
+
+    useEffect(() => {
+        const timeout = triggAnimation && setTimeout(() => {
+            setTriggAnimation(false)
+        }, 500)
+        return () => clearTimeout(timeout);
+    }, [triggAnimation])
 
     return (
         <nav className="navbar">
@@ -16,12 +34,13 @@ export const Navbar = ({ isMobile, cartCount, setCartDisplayed }) => {
                 <h3>Game Store</h3>
             </Link>}
             <SearchBar />
-            {!isMobile && <a className="githubContainer" target="_blank" href="https://github.com/marcofs10/gamestore">
-                <img src={github} />
-                <h3>marcofs10</h3>
-            </a>}
-            <div className='cartContainer' onClick={handleOpenCart}>
-                <img src={cart} className='cart'/>
+            {!isMobile &&
+                <a className="githubContainer" target="_blank" href="https://github.com/marcofs10/gamestore">
+                    <img src={github} />
+                    <h3>marcofs10</h3>
+                </a>}
+            <div className={`cartContainer ${triggAnimation ? 'animation' : ''}`} onClick={handleOpenCart}>
+                <img src={cart} className={`cart ${triggAnimation ? 'animationCart' : ''}`} />
                 <h3>{`Cart: ${cartCount}`}</h3>
             </div>
         </nav>
